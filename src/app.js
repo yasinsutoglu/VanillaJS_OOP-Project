9e4718b81c7100bcce7f448163e62d8f22751117
Yasin Sutoglu <ysutoglu@gmail.com>
2023-01-18 15:25:02 +0300

    show alert done

--- src/app.js
+++ src/app.js
@@ -42,6 +42,21 @@ class UI {
         }
     }
 
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message))
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form)
+
+        //vanish in 2 seconds
+        setTimeout(()=>{
+            document.querySelector('.alert').remove()
+        }, 2000)
+    }
+
     static clearFields(){
         document.querySelector("#title").value = "";
         document.querySelector('#author').value = '';
@@ -63,18 +78,28 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>{
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector("#isbn").value;
 
-    //Instantiate Book
-    const book = new Book(title,author,isbn);
+    //Validate
+    if(!title || !author || !isbn){
+        UI.showAlert('Please fill in all fields' , 'warning')
+    }else{
+      //Instantiate Book
+      const book = new Book(title, author, isbn);
 
-    // Add Book to UI
-    UI.addBookToList(book)
+      // Add Book to UI
+      UI.addBookToList(book);
 
-    //clear form fields
-    UI.clearFields()
-    //* document.querySelector("#book-form").reset() --> alternative solution
+      //Show success message
+      UI.showAlert('Book added!' , 'success')
+
+      //clear form fields
+      UI.clearFields();
+      //* document.querySelector("#book-form").reset() --> alternative solution
+    }    
 })
 
 //!Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click' , (e)=>{
-    UI.deleteBook(e.target)
+  UI.deleteBook(e.target);
+  //Show success message
+  UI.showAlert("Book deleted!", "success");
 })
