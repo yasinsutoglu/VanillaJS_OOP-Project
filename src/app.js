d332b9c0e94d4cd85a5411ee4bc0d6f5fec7bf9c
Yasin Sutoglu <ysutoglu@gmail.com>
2023-01-18 15:41:46 +0300

    project done

--- src/app.js
+++ src/app.js
@@ -10,13 +10,7 @@ class Book{
 //!UI Class: Handle UI Tasks
 class UI {
     static displayBooks(){
-        const storedBooks = [{
-            title: "misserables",
-            author: "yaso",
-            isbn: '35215234'
-        }];
-
-        const books = storedBooks;
+        const books = Store.getBooks()
 
         books.forEach((book)=> UI.addBookToList(book) )
     }
@@ -65,6 +59,37 @@ class UI {
 }
 
 //! Store Class: Handles Storage
+class Store {
+  static getBooks() {
+    let books = null;
+    if(localStorage.getItem('books') === null){
+        books = [];
+    }else{
+        books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks()
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook(isbn) {
+    let books = Store.getBooks();
+
+    //* books = books.filter((book)=> book.isbn !== isbn) --> alternative solution
+    books.forEach((book,index)=>{
+        if(book.isbn === isbn){
+            books.splice(index,1)
+        }
+    });
+    
+    localStorage.setItem('books' , JSON.stringify(books))
+    
+  }
+}
 
 //! Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
@@ -88,6 +113,9 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>{
       // Add Book to UI
       UI.addBookToList(book);
 
+      //Add book to Store
+      Store.addBook(book)
+
       //Show success message
       UI.showAlert('Book added!' , 'success')
 
@@ -99,7 +127,12 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>{
 
 //!Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click' , (e)=>{
+    //Remove book form UI
   UI.deleteBook(e.target);
+
+  //Remove book from Store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+  
   //Show success message
   UI.showAlert("Book deleted!", "success");
 })
